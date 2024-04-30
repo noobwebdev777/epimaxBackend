fdfc79fc4f146976a56b6bddb887ee0ec1551518
noobwebdev777 <125580549+noobwebdev777@users.noreply.github.com>
2024-04-30 23:32:35 +0530

    database conection pend

---
+++ src/database/database.js
@@ -0,0 +1,12 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'me',
+  password : 'secret',
+  database : 'my_db'
+});
+
+connection.connect();
+
+module.exports = database
+
--- src/server/main.js
+++ src/server/main.js
@@ -1,19 +1,24 @@
 const express = require('express');
+require('dotenv').config();
 
-const auth = require('middleware/auth');
+// const database = require('../database/database')
+
+const auth = require('../middleware/auth');
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // app.use(auth);
+
+// app.use(express.json)
 
 //api endpoint to register a user sending username and password in a json format
 
 app.get('/', auth, (req, res) => {
   try {
     console.log('request recived at get-""/"');
-    res.status(200).json([{}]);
+    res.status(200).json('');
   } catch (e) {
     console.log(e);
     res.status(500).json([{ message: 'dont know what error is this' }]);
